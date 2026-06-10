347fbdf22e846b1b9fa8056303a8a663708f06ed
MellowMelloside <163877467+MackanzieWijaya@users.noreply.github.com>
2026-06-10 15:28:22 +0800

    setting the cookies to not have httpOnly and sameSite Strict for vulnerability testing

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly: true,
-        sameSite:"strict"
+        httpOnly: false,
+        sameSite:"lax"
     }
 }));
 
